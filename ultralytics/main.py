48509938eee4416dfa8b3043c1f3a9f84f9ee33e
pgsweet <113042887+pgsweet@users.noreply.github.com>
2026-05-13 22:43:53 -0400

    more code

--- ultralytics/main.py
+++ ultralytics/main.py
@@ -1,5 +1,21 @@
 from ultralytics import YOLO
+import time
+import cv2
 
-model = YOLO("yolov8n.pt")
+for attempt in range(20):
+    cap = cv2.VideoCapture(0)
+    if cap.isOpened():
+        ret, frame = cap.read()
+        cap.release()
+        if ret:
+            print("Camera ready")
+            break
+
+    print("Waiting for camera...")
+    time.sleep(2)
+else:
+    raise RuntimeError("Camera not found or not ready")
+
+model = YOLO("yolov8s.engine")
 
 model.track(source=0, device=0, show=True, tracker="bytetrack.yaml", persist=True, imgsz=1080, classes=[0])
